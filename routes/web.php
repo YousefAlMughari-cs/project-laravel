9499eb7daa6d608c429374d1e6ad5a00b8143916
Yousef al mughari <yousefallmughari@gmail.com>
2025-04-13 15:53:54 +0300

    Route&Controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,32 @@
 <?php
 
+use App\Http\Controllers\SiteOne\SiteOneController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', function () {
-    return view('home');
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+// Route::get('/news/{id?}', function ($id = null) {
+//     $news = [1,2,3,4,5,6];
+//     if($id) {
+//         echo "News ID: $id";
+//     }else {
+//         echo "all news ".'<br>';
+//         foreach ($news as $new) {
+//             echo  $new ."<br>";
+//         }
+//     }
+// });
+
+
+Route::prefix('site2')->controller(SiteOneController::class)->group(function() {
+    Route::get('/home','home');
+    Route::get('/about','about');
+    Route::get('/contact','contact');
+    Route::get('/msg/{id}','msg');
 });
